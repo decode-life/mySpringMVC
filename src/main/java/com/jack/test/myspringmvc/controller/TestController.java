77e78c588837e5ef3f8f93f1498b4890e6cae305
decodeLife <1071657658@qq.com>
2018-07-18 21:41:03 +0800

    添加忽略文件

--- src/main/java/com/jack/test/myspringmvc/controller/TestController.java
+++ src/main/java/com/jack/test/myspringmvc/controller/TestController.java
@@ -2,10 +2,15 @@ package com.jack.test.myspringmvc.controller;
 
 import com.jack.test.myspringmvc.annotation.MyController;
 import com.jack.test.myspringmvc.annotation.MyRequestMapping;
+import sun.tools.attach.HotSpotVirtualMachine;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 
 /**
  * Created by jie on 2018/4/5.
@@ -18,6 +23,7 @@ public class TestController {
     public void one(HttpServletRequest request, HttpServletResponse response, String name){
         System.out.println("test param name = " + name);
         try {
+            SftpUtil.getMyObj(1, name);
             response.getWriter().write("传入参数name:" + name);
         } catch (IOException e) {
             e.printStackTrace();
